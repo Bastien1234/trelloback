59224a14f30a0eabc5ae97e37ad1325728faf988
unknown <bastien.clementi@gmail.com>
2022-06-09 09:10:08 +0200

    mind the dotenv

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,9 @@
+const Users = require('./../models/userModels');
+
+const log = (args) => {
+    console.log(args);
+}
+
 exports.testServer = async (req, res, next) => {
     try {
         res.status(200).json({
@@ -10,6 +16,36 @@ exports.testServer = async (req, res, next) => {
         res.status(400).json({
             status: "failed",
             data: "It has failed !!!"
+        })
+    }
+
+    next();
+}
+
+exports.deleteUser = async(req, res, next) => {
+
+    try {
+        const id = req.params.id;
+        const document = await Users.findByIdAndDelete(id);
+
+        if (!document)
+        {
+            res.status(404).json({
+                status: 'failed',
+                message: 'Document not found'
+            })
+        }
+
+        res.status(204).json({
+            status: 'success',
+            data: null
+        })
+    }
+    
+    catch (err) {
+        res.status(400).json({
+            status: 'failed',
+            data: err
         })
     }
 
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ process.on('uncaughtException', err => {
     console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
     console.log(err.name, err.message);
     process.exit(1);
-  });
+});
 
 process.on('unhandledRejection', err => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
